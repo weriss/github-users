39fd6303dcde8539f5a591b647960d99e4581f3a
Weriss <49867131+weriss@users.noreply.github.com>
2019-05-18 23:46:04 +0800

    nice
    import LinearGradient from 'react-native-linear-gradient'
    import React,{Component } from 'react'
    import {ListItem} from  'react-native-elements'
    import {Text, StyleSheet, Dimensions, FlatList, View,Image, Button, Alert, TextInput,ToastAndroid,LayoutAnimation,} from 'react-native'
    import { createStackNavigator, createAppContainer } from "react-navigation";
    import { TouchableOpacity } from 'react-native-gesture-handler';
    import  {DeviceEventEmitter} from 'react-native';

--- App.js
+++ App.js
@@ -1,255 +1,271 @@
+import LinearGradient from 'react-native-linear-gradient'
+import React,{Component } from 'react'
+import {ListItem} from  'react-native-elements'
+import {Text, StyleSheet, Dimensions, FlatList, View,Image, Button, Alert, TextInput,ToastAndroid,LayoutAnimation,} from 'react-native'
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import  {DeviceEventEmitter} from 'react-native';
 
-import React, { Component } from 'react';
-import SwipeRow from './SwipeRow'
-import {
-StyleSheet,
-View,
-Image,
-Dimensions,
-TouchableOpacity,
-FlatList,
-Text,
-Button,
-Alert,
-ToastAndroid,
-TouchableHighlight,
-TextInput,
-LayoutAnimation,
-Animated
-} from 'react-native';
+class HomeScreen extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={users:[],text:'',isSelect:-1};
+  }
+  static navigationOptions = {
+    header:null,
+  };
 
-var users = [
-  {   key:'1',
-      login: 'mojomb',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-      name: 'Tom Preston-Werner',
-      blog: 'http://tom.preston.werner.com',
-      location:'San Francisco',
-  },
-  {   key:'2',
-      login: 'defunkt',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-      name: 'Chris Wanstrath',
-      blog: 'http://chriswanstrath.com/',
-      location:'San Francisco',
-  },
-  {   key:'3',
-      login: 'pjhyett',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-      name: 'PJ Hyett',
-      blog: 'https://hyett.com',
-      location:'San Francisco',
-  },
-  {   key:'4',
-      login: 'wycats',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/4?v=4',
-      name: 'Yehuda Katz',
-      blog: 'http://yehudakatz.com',
-      location:'San Francisco',
-  },
-  {   key:'5',
-      login: 'ezmobius',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/5?v=4',
-      name: 'Ezra Zygmuntowicz',
-      blog: 'http://stuffstr.com',
-      location:'San Francisco',
-  },
-  {   key:'6',
-      login: 'ivey',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/6?v=4',
-      name: 'Micheal D. Ivey',
-      blog: 'http://gweezlebur.com',
-      location:'Bay Minette, AL',
-  },
-  {   key:'7',
-      login: 'evanphx',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/7?v=4',
-      name: 'Evan Phoenix',
-      blog: 'http://blog.fallingsnow.net',
-      location:'Los Angeles, CA',
-  },
-  {   key:'8',
-      login: 'vanpelt',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/17?v=4',
-      name: 'Chris Van Pelt',
-      blog: 'vandev.com',
-      location:'San Francisco',
-  },
-  {   key:'9',
-      login: 'wayneeseguin',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/18?v=4',
-      name: 'wayne E. Seguin',
-      blog: '',
-      location:'Buffalo, NY',
-  },  
-  {   key:'10',
-      login: 'brynary',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/19?v=4',
-      name: 'Bryan Helmkamp',
-      blog: 'http://codeclimate.com',
-      location:'New York City',
-  },
-  {   key:'11',
-      login: 'kevinclark',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/20?v=4',
-      name: 'Kevin Clark',
-      blog: 'http://glu.ttono.com',
-      location: null,
-  },
-]
+    componentDidMount(){
+      fetch('https://api.github.com/users')
+        .then( response => response.json())
+        .then( data => this.setState({
+          users:data
+        }))
+        .catch( error => alert(error) )
+        DeviceEventEmitter.addListener("details", (callback) => {
+          this.setState({users:callback})
+      })
+    }
 
-export default class App extends Component {
+    render(){
+      return(
+        <View style={styles.container}>
+          <LinearGradient
+            start={{ x : 0.0, y : 0.0 }} end={{ x : 1.0, y : 1.0 }}
+            locations={[0,0.3,0.5,0.8,1]}
+            colors={['#252652', '#4A3560', '#815D67', '#880A47', '#650142']}
+            // colors={['#252652', '#815D67']}
+            style={styles.viewTitle}>
+            
+              <Text style={styles.textTitle}>
+                  Users Of Github
+              </Text>
+          </LinearGradient>
+          <FlatList 
+          renderItem = {this._renderItem}
+          data = {this.state.users}
+          getItemLayout={(data,index)=>({length:100,offset:(100+2)*index,index})}
+          keyExtractor={(item,index)=>String(index)}
+          ItemSeparatorComponent={this._itemSeparator}
+          >
+          </FlatList>
+      </View>
+      )
+    }
+    _renderItem=(item)=>(
+      <View style={styles.itemBox}>
+        
+      <ListItem
+        title={item.item.login}
+        subtitle={item.item.type}
+        leftAvatar={{rounded:true,source: item.item.avatar_url && { uri: item.item.avatar_url }}}
+        style={styles.itemBox}
+        // rightTitle='>'
+        rightTitle=
+        {
+          <TouchableOpacity 
+            onPress={()=>{
+              this.props.navigation.navigate('Details',
+                {param1:this.state.users,
+                   param2:item.index,
+                    }
+                    )}} 
+            style={styles.rightButton}>
+            <Text style={fontSize=30}>></Text>
+          </TouchableOpacity>
+        }
+        onLongPress={()=>{Alert.alert('Warning','Do you want to delete it?',
+          [
+            {text:'No'},
+            {text:'Yes',onPress:()=>{
+              if(this.state.users.length==1){
+                    this.setState({
+                      users:[]
+                    })
+                  }
+              else{
+                this.state.users.splice(item.index,1);
+                this.setState(
+                  {users:this.state.users});}
+            }}
+          ])
+        }
+        }
+        >
+        </ListItem>
+      </View>
+    )
+    _itemSeparator(){
+      return(
+      <View
+        style={{
+        height: 1,
+        width: "90%",
+        backgroundColor: "#CED0CE",
+        marginLeft: "14%"
+        }}
+        />
+      )
+    }
+    // _passToDetails(index){
+
+    // }
+}
+
+class DetailsScreen extends React.Component {
   constructor(props){
     super(props)
-    this.state={data:users,text:'',isSelect:-1}
+    // const{navigation}= this.props
+    this.state={
+      user:this.props.navigation.getParam('param1'),
+      order:this.props.navigation.getParam('param2'),
+      isSelect:-1,
+    }
   }
-  itemTap=(index)=>{
-    let select=index;
-    if(this.state.isSelect===index){
-      select=-1;
-    }
-  LayoutAnimation.easeInEaseOut();
-  this.setState({
-    isSelect:select
-  })
-}
-     change=(index,text,f)=>{
-      let cc=[...this.state.data]
-      if(f===1)
-        cc[index].name=text;
-      else if(f===2)
-        cc[index].blog=text;
-      else if(f==3)
-        cc[index].location=text;
-      else
-        cc[index].login=text;
-      this.setState({data:cc})
-     }
-
-      render(){
-        return(
-          <View style={styles.view}>
-             <FlatList
-                  data={this.state.data}
-                  renderItem={this._renderItem}
-                  onEndReachedThreshold={0.1}
-                  onEndReached={this._onload}
-                  onRefresh={this.refreshing}
-                  refreshing={false}
-                  getItemLayout={(data,index)=>({length: 100, offset: (100+2) * index, index})}
-                  ItemSeparatorComponent={()=><View style={{width:Dimensions.get('window').width,height:2,backgroundColor:'black'}}></View>}>
-              </FlatList>
+  static navigationOptions = {
+    headerStyle: {
+      backgroundColor: '#4A3560',
+    },
+    headerHintColor:'#815D67'
+  };
+  render(){
+    return (
+      <View style={styles.containerDetails}>
+        <View style={styles.boxView1}>
+          <View  style={styles.imgBackground}>
+        {/* <Text>{this.state.user[this.state.order].login}</Text> */}
+          <Image source={{uri:this.state.user[this.state.order].avatar_url}} style={styles.imgStyle}></Image>
           </View>
-          );
-      }
-_renderItem = (item) => {
-        return (
- <View style={styles.container}>
-  <SwipeRow>
-    <TouchableOpacity style={styles.delTextContainer} onPress={()=>{Alert.alert ('Warnning:','Do you want to delete it?',
-                [
-                      {text:'No'},
-                      {text:'Yes',onPress:()=>{
-                          this.state.data.splice(parseInt(item.index),1);
-                          for(let i=0;i<this.state.data.length;i++)
-                            this.state.data[i].key=(i+1).toString();
-                          this.setState(
-                            {data:this.state.data});
-                            ToastAndroid.show('Delete.',ToastAndroid.SHORT);
-                          }
-                      }
-                ])}}> 
-                  <Text style={styles.text}>DELETE</Text>
-      </TouchableOpacity>
-      <View style={{flexDirection:'column',width:Dimensions.get('window').width,backgroundColor:'white'}}>    
-        <View style={styles.view}>
-          <Image style={styles.image} source={{uri:item.item.avatar_url}}/>
-            <View style={styles.text}>
-              <Text style={styles.text}>{item.item.login}</Text> 
-            </View>
-            <View style={flex=2}>
-              <TouchableOpacity style={{height:120,backgroundColor:'black'}} onPress={()=>{this.itemTap(item.index)}}>
-                <Text style={[styles.text,{color:'white'}]}> More</Text>
-              </TouchableOpacity>
-            </View>
-         </View>
-      
-      {this.state.isSelect === item.index ?
-        <View style={styles.textbox}>
-          <TextInput style={styles.textinput} placeholder={'Name: '+item.item.name} onEndEditing={(event)=> this.change(item.index,event.nativeEvent.text,1)}></TextInput>
-          <TextInput style={styles.textinput} placeholder={'Blog: '+item.item.blog} onEndEditing={(event)=> this.change(item.index,event.nativeEvent.text,2)}></TextInput>
-          <TextInput style={styles.textinput} placeholder={'Location: '+item.item.location} onEndEditing={(event)=> this.change(item.index,event.nativeEvent.text,3)}></TextInput>
-          <TextInput style={styles.textinput} placeholder={'Login: '+item.item.login} onEndEditing={(event)=> this.change(item.index,event.nativeEvent.text,4)}></TextInput>
+        </View>
+        <View style={styles.boxView2}>
           
-        </View>:null
-      }
-      
+            <Text style={styles.LoginText}>{this.state.user[this.state.order].login}</Text>
+            <TouchableOpacity onPress={this.changeIsSelect}>
+              <Image source={require('./edit.png')} style={{width:20, height:20, backgroundColor:"transparent"}} ></Image>
+            </TouchableOpacity>
+            {this.state.isSelect===-1? null:
+            <TextInput style={styles.inputText} autoFocus={true} onEndEditing={(event)=> this.changeLogin(this.state.order,event.nativeEvent.text)}></TextInput>
+            }
+            <Text style={styles.typeText}>{this.state.user[this.state.order].type}</Text>
+        </View>
+        <View style={styles.boxView3}>
+            <Text style={styles.bottomText}>https://api.github.com/users/{this.state.user[this.state.order].login}</Text>
+        </View>
       </View>
-    </SwipeRow>
-  </View>
-        );
-    }
-
-    
-    _onload(){
-        let timer =  setTimeout(()=>{
-            clearTimeout(timer)
-        },1500)
-    }
-    refreshing(){
-        let timer =  setTimeout(()=>{
-            clearTimeout(timer)
-            ToastAndroid.show('refreshed',ToastAndroid.SHORT);
-        },1500)
-    }
+    )
+  }
+  changeIsSelect=()=>{
+    let is=1;
+    this.setState({isSelect:is});
+  }
+  changeLogin=(index,text)=>{
+    let cc=[...this.state.user]; 
+    let is=-1;
+    cc[index].login=text;
+    this.setState({user:cc,isSelect:is});
+    DeviceEventEmitter.emit('details',cc);
+  }
 }
-  const styles=  StyleSheet.create({
 
-    text: {
-        flex:1,
-        textAlign: 'center',
-        textAlignVertical: 'center',
-        color: 'black',
-        fontSize:20
-    },
-
-    view:{
-        flex:1,
-        flexWrap:'wrap',
-        flexDirection:'row',
-        backgroundColor:'white',
-    },
-
-    image:{
-        flex:1,
-        width:150,
-        flexWrap:'wrap',
-    },
-
-　　textbox:{
-        flexDirection: 'column',
-        alignItems:'center',
-        width:Dimensions.get('window').width,
-        borderRadius:5,
-        height:80,
-        flex:1,
-    },
-
-    textinput:{
-        height:20,
-        padding:0,
-    },
-
-    delTextContainer:{
-        width: 100,
-        height:120,
-        backgroundColor: 'red',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-
-    container:{
-      width:Dimensions.get('window').width,
-      backgroundColor:'pink',
-    },
-})
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: "Home"
+    // initialRouteName:'Details'
+  },
+);
+const Width=Dimensions.get('window').width;
+const Height=Dimensions.get('window').height;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  containerDetails:{
+    width:Width,
+    height:Height,
+    backgroundColor:'#650142',
+    flexDirection:'column',
+    // justifyContent:'space-around'
+  },
+  textTitle:{
+    fontSize:20,
+    color:'white',
+  },
+  viewTitle:{
+    width:Dimensions.get('window').width,
+    height:100,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor: 'black',
+  },
+  itemBox:{
+    // backgroundColor:'white',
+    // height:100,
+    width:Dimensions.get('window').width-20,
+    flexDirection:'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:'black',
+  },
+  imgStyle:{
+    width:150,
+    height:150,
+    borderRadius:250,
+  },
+  imgBackground:{
+    height:180,
+    width:180,
+    borderRadius:250,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#815D67'
+  },
+  boxView1:{
+    justifyContent:'center',
+    alignItems:'center',
+    width:Width,
+    height:Height*0.3,
+    marginBottom:10,
+  },
+  boxView2:{
+    justifyContent:'flex-start',
+    alignItems:'center',
+    width:Width,
+    height:Height*0.5,
+  },
+  boxView3:{
+    justifyContent:'flex-start',
+    alignItems:'center',
+    width:Width,
+    height:Height*0.1,
+  },
+  bottomText:{
+    fontSize:15,
+    color:'#CB81B1'
+  },
+  LoginText:{
+    fontSize:36,
+    color:'#CB81B1'
+  },
+  typeText:{
+    fontSize:20,
+    color:'#815D67'
+  },
+  editImg:{
+    width:20,
+    height:20,
+    marginBottom:10
+  },
+  inputText:{
+    marginTop:10,
+    width:50,
+    height:50,
+  }
+});
+export default createAppContainer(AppNavigator);
+//可以显示渐变，列表，页面跳转
